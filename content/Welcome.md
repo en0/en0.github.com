bd2a00ff5d21d01ea645e9de35e13ffdb19ed5bd
Ian Laird <irlaird@velma.com>
2016-04-12 00:16:17 -0600

    Initial Commit

---
+++ content/Welcome.md
@@ -0,0 +1,31 @@
+Title: Welcome
+Date: 2016-04-11 23:00
+Tags: other
+Category: Other
+Slug: other
+Author: Ian Laird
+Summary: What I do.
+
+# Let's Get Rolling.
+I have been toying with the idea of posting my thoughts and
+discoveries for many years now. I have never really had the time or the guts to
+do it.  Until recently, I have been content just doing what I do and sharing
+my skills with my friends and coworkers.
+
+Lately, I have found myself wishing i had written things down. There are so
+many fine details in any technical job it begins to get difficult to keep
+everything organized and clear in your mind. 
+
+This has driven me to start this blog.  I do not have any content yet. I will
+not promise any content soon.  This blog is mostly for myself: to catalog my
+thoughts and share ideas with people that ask.
+
+## Who am I?
+Well, let us keep it superficial for now. I am a software engineer. I write
+code because I enjoy and people pay me to.  It's a pretty good situation and I
+feel very lucky to do something I enjoy to provide for my family. I spend time
+as far away from the user interface as possible, unless my user lives in bash.
+I am of the firm opinion that your really only need 2 program languages to solve
+most real world software problems, C and Python.
+
+That is all for now.
---
+++ pelicanconf.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Ian Laird'
+SITENAME = u'IanRLaird.com'
+# SITESUBTITLE = ""
+SITEURL = ''
+
+GITHUB_USERNAME = "en0"
+BLOG_START_YEAR = "2016"
+
+PATH = 'content'
+
+TIMEZONE = 'America/Boise'
+
+DEFAULT_LANG = u'en'
+
+THEME = './theme/lazystrap'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/en0'),
+        ('Linkedin', 'https://www.linkedin.com/in/ian-laird-64916131'),)
+
+DEFAULT_PAGINATION = 10
+
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+ARTICLE_URL = 'blog/{slug}'
+ARTICLE_SAVE_AS = 'blog/{slug}/index.html'
+ARCHIVES_SAVE_AS = 'archives/index.html'
+PAGINATED_DIRECT_TEMPLATES = ('archives',)
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page{number}/', '{base_name}/page{number}/index.html')
+)
+
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
