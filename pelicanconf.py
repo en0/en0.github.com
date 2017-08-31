fc4def2b3b5dae62b5aeb5ec4d9ca6571014287a
Ian Laird <irlaird@gmail.com>
2017-08-30 22:15:25 -0600

    Fixed config issues causing author links to be broken. Also added custom css for headers.

--- pelicanconf.py
+++ pelicanconf.py
@@ -37,8 +37,10 @@ SOCIAL = (('Github', 'https://github.com/en0'),
 
 DEFAULT_PAGINATION = 10
 
-TAG_URL = 'tag/{slug}'
-TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAG_URL = 'tag/{slug}/index.html'
+TAG_SAVE_AS = 'tag/{slug}'
+AUTHOR_URL = 'author/{slug}/index.html'
+AUTHOR_SAVE_AS = 'author/{slug}'
 ARTICLE_URL = 'blog/{slug}'
 ARTICLE_SAVE_AS = 'blog/{slug}/index.html'
 ARCHIVES_SAVE_AS = 'archives/index.html'
